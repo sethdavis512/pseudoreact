1b0315d87337aede8cf518ddfe8b388e1be3612a
Seth Davis <sethdavis512@gmail.com>
2021-12-24 14:36:11 -0600

    Initial files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useCallback, useState } from 'react';
+import CodeMirror from 'react-codemirror';
+// @ts-ignore
+import * as espree from 'espree';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/jsx/jsx';
+
+const useParsePseudo = (pseudoCode: string): any => {
+    const ast = espree.parse(pseudoCode, {
+        ecmaVersion: 6,
+        ecmaFeatures: {
+            jsx: true
+        }
+    });
+
+    return ast;
+};
+
+const App = () => {
+    const [pseudoCode, setPseudoCode] = useState(
+        `<Layout>\n    <Header />\n    <Main />\n    <Footer />\n</Layout>`
+    );
+
+    const ast = useParsePseudo(pseudoCode);
+    console.log(ast);
+
+    const handleEditorChange = useCallback(
+        (text: string): void => setPseudoCode(text),
+        []
+    );
+
+    const handleSave = useCallback((event: FormEvent): void => {
+        event.preventDefault();
+    }, []);
+
+    const editorOptions = { lineNumbers: true };
+
+    return (
+        <div>
+            <h1 className="text-2xl">Pseudopia</h1>
+            <div className="grid md:grid-cols-2 gap-2">
+                <form onSubmit={handleSave}>
+                    <CodeMirror
+                        autoFocus
+                        className="border-2 border-slate-800"
+                        name="pseudoCode"
+                        onChange={handleEditorChange}
+                        options={editorOptions}
+                        value={pseudoCode}
+                    />
+                    <button
+                        className="bg-slate-800 text-white border-2 border-slate-800 py-2 px-4 rounded-md"
+                        type="submit"
+                    >
+                        Save
+                    </button>
+                </form>
+                <div>Other stuff</div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
