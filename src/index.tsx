5f253bd1fd50503eb732db2ed4709c6bafd06901
Seth Davis <sethdavis512@gmail.com>
2021-12-29 22:31:53 -0600

    Report vitals to GA

--- src/index.tsx
+++ src/index.tsx
@@ -23,4 +23,15 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+// @ts-ignore
+function sendToAnalytics({ id, name, value }) {
+    ReactGA.ga({
+        eventCategory: 'Web Vitals',
+        eventAction: name,
+        eventValue: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+        eventLabel: id, // id unique to current page load
+        nonInteraction: true, // avoids affecting bounce rate
+    });
+}
+
+reportWebVitals(sendToAnalytics);
